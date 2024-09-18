6bbb57e5f4785ca1743e3d01605bb19b65159926
patrickTHEJP <115226385+patrickTHEJP@users.noreply.github.com>
2024-09-18 22:48:00 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,54 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Counter from './components/Counter';
+import NotFound from './components/NotFound';
+
+const App = () => {
+  return (
+    <Router>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+          <li><Link to="/counter">Counter</Link></li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/counter" element={<Counter />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
